1494fb7adbcc1ec556081a2bc741828305aa8da7
Gabriel Maresch <gabriel.maresch@gmail.com>
2026-05-05 17:37:01 +0200

    simplify interaction Hamiltonian for hermitian A; diagonal density matrix realization via sampling; helper functions for evolutino operator parameter extraction; implemented correct and readable labeling for circuit diagram

--- gs_prep.py
+++ gs_prep.py
@@ -1,5 +1,6 @@
 import pennylane as qml
 import pennylane.numpy as np
+
 import random
 from typing import Any, List
 import matplotlib.pyplot as plt
@@ -42,19 +43,24 @@ def construct_opset(num_qubits:int, *, output:bool = False):
 
 
 
-def construct_interaction_hamiltonian(num_qubits:int, sigma:float, t:float,  A:"qml.Operator", alpha:float = 1):
+def construct_interaction_hamiltonian(num_qubits:int, sigma:float, t:float,  A:"qml.Operator", alpha:float = 1, *, hermitian:bool = True):
     # num_qubits is system qubits without environment
 
     
     with qml.QueuingManager.stop_recording():
         # environment
         B = (qml.PauliX(num_qubits) - 1j * qml.PauliY(num_qubits)) / 2
+        # B_dagger = (qml.PauliX(num_qubits) + 1j * qml.PauliY(num_qubits)) / 2
         f = evaluate_sqrt_gaussian(sigma, t)
-
-        ops = [A@B, qml.adjoint(A@B)]
+        
+        if hermitian:
+            return qml.Hamiltonian([alpha*f], [A@qml.PauliX(num_qubits)]) 
+        else:
+            ops = [A@B, qml.adjoint(A@B)]
+        
         coeffs = [alpha*f, alpha*f]
         H_int = qml.Hamiltonian(coeffs, ops)
-    #return f(AB*A†B)
+    
     return H_int
 
 def construct_environmental_hamiltonian(num_qubits:int, omega:float):
@@ -64,39 +70,77 @@ def construct_environmental_hamiltonian(num_qubits:int, omega:float):
 def construct_W_layer(num_qubits:int, m:int, tau:float, T:float, sigma:float, op:"qml.Operator", alpha:float = 1):
     t = (m+1)/2*tau-T
     H_int = construct_interaction_hamiltonian(num_qubits, sigma, t, op, alpha)
-    qml.evolve(H_int, coeff=tau/2)
+    
+    #probably better to make this time evolution explicit
+    #qml.ApproxTimeEvolution(H_int, tau, 1)
+    qml.evolve(H_int, coeff=tau)
+    
     return None
 
-def construct_U_layer(num_qubits:int, m:int, tau:float, T:float, sigma:float, op:"qml.Operator", omega:float, H_sys:qml.Hamiltonian, alpha:float = 1):
-
-    construct_W_layer(N, 1, tau, T, sigma, op, alpha )
+def construct_U_layers(num_qubits:int, tau:float, T:float, sigma:float, op:"qml.Operator", omega:float, H_sys:qml.Hamiltonian, alpha:float = 1):
     
+    M = int(np.ceil(2*T/tau)) # number of substeps  
+    # print(M, "substeps")
+ 
     H_env = construct_environmental_hamiltonian(num_qubits, omega)
-    qml.evolve(H_env, coeff=tau)
-    # this should later be trotterized
-    qml.evolve(H_sys, coeff=tau)
+    
+    construct_W_layer(N, 1, tau/2, T, sigma, op, alpha )
+    for i in range(M):
+        #qml.ApproxTimeEvolution(H_env, tau, 1)
+        qml.evolve(H_env, coeff=tau)
+        # this should later be trotterized
 
-    construct_W_layer(N, 1, tau, T, sigma, op, alpha )
+        #qml.ApproxTimeEvolution(H_sys, tau, 1)
+        qml.evolve(H_sys, coeff=tau)
+        if i < M-1:
+            construct_W_layer(N, 1, tau, T, sigma, op, alpha )
+        else:
+            construct_W_layer(N, 1, tau/2, T, sigma, op, alpha )
     
     return None
 
 
-def initialize_rho_env(wire:int, beta:float, omega:float, *, first:bool=False):
-    if not first: 
-        _ = qml.measure(wires=wire, reset=True)
+def initialize_rho_env(wire:int, beta:float, omega:float, rng:int = 42, *, first:bool = False):
+    
+    if not first:
+        qml.measure(wires=[wire], reset=True)
+    
     Z = np.exp(omega*beta/2) + np.exp(-omega*beta/2)
-    rho_env = np.array([(np.exp(omega*beta/2)/Z)**(1/2), (np.exp(-omega*beta/2)/Z)**(1/2)], dtype=complex)  
-    qml.StatePrep(rho_env, wires=[wire])
+    p0, p1 = np.exp(omega*beta/2)/Z, np.exp(-omega*beta/2)/Z
+    
+
+    # exp(beta Z) is diagonal, so we sample from the density matrix
+    random.seed(rng)
+    if random.uniform(0.0, 1.0) > p0:
+        qml.PauliX(wires=wire)
+    #rho_env = np.diag(np.array([p0, p1], dtype=complex)) 
+    #qml.QubitDensityMatrix(rho_env, wires=[wire])
 
     return None
 
-def sample_A_omega(op_set:List["qml.Operator"] , omega_max:float, rng:int = 24):
+def sample_operator(op_set:List["qml.Operator"], rng:int = 24):
     random.seed(rng)
     A = random.choice(op_set)
-    omega = random.uniform(0, omega_max)
     print("chosen operator: ", A)
+    return A
+
+def sample_omega(omega_max:float, rng:int = 24):
+    random.seed(rng)
+    omega = random.uniform(0, omega_max)
     print("chosen omega:", np.round(omega, 2))
-    return A, omega
+    return omega
+
+
+############### helper function ###########
+def get_evolutiontime(op:"qml.Hamiltonian"):
+    if isinstance(op, qml.ApproxTimeEvolution):
+        evo_time = float(op.parameters[0])
+        print(evo_time)
+    elif isinstance(op, qml.ops.op_math.Evolution):
+        evo_time = np.abs(op.coeff)
+    return evo_time
+
+
 
 if __name__ == "__main__":
 
@@ -108,58 +152,74 @@ if __name__ == "__main__":
     omega_max = 5
     beta = 10 
 
-
-    tau = 0.5
-    M = int(np.ceil(2*T/tau))
-    print(M, "substeps")
+    tau = 1/2
+    num_timesteps = 2
 
     op_set = construct_opset(N)
-    A, omega = sample_A_omega(op_set, omega_max)
+    A, omega = sample_operator(op_set), sample_omega(omega_max)
 
     H_sys  = transverse_ising_hamiltonian(-1, 0, N)
 
-    
-
-
-
-    dev = qml.device("default.qubit", wires=N+1)
-    @qml.qnode(dev)
+    # we might need a mixed device, because the auxilary qubit is thermal 
+    dev = qml.device("default.qubit", wires=N+num_timesteps)
+    @qml.qnode(dev) #we need extra qubits for deferred mid circuit measurment
 
     def circuit():
         
-        #construct_W_layer(N, 1, tau, T, sigma, op_set, alpha )
-
-        initialize_rho_env(N, beta, omega, first=True)
-        qml.Barrier(wires=range(N + 1), only_visual=True)
-
-        for _ in range(M):
-            construct_U_layer(N, 1, tau, T, sigma, A, omega, H_sys, alpha)
+        for i in range(num_timesteps):
+            is_first = (i==0)
+            initialize_rho_env(N, beta, omega, first=is_first)
+            construct_U_layers(N, tau, T, sigma, A, omega, H_sys)
+            #qml.measure(wires=N_env)
             qml.Barrier(wires=range(N + 1), only_visual=True)
-
-
         
         return qml.density_matrix(wires=range(N))
-
+            
     final_state = circuit()
     print(np.round(final_state, 4))
 
 
-
+    
 
 
     ###################### display circuit nicely ###################
-    display_circuit = False
+    display_circuit = True
     if display_circuit:
         from matplotlib.text import Text
+            
+        qscript = qml.tape.make_qscript(circuit.func)()
+        all_ops = list(qscript.operations)
+
+        evolv_ops = {"W_tau": [], "W_tau_half": [], "H_env": [], "H_sys": []}
+        for op in all_ops:
+            #print(op.name, list(op.wires), getattr(op, "coeff", None))
+            if "Evolution" in op.name:
+                if [N] == list(op.wires):
+                    evolv_ops["H_env"].append(op)
+                elif N not in list(op.wires):
+                    evolv_ops["H_sys"].append(op)
+                elif np.isclose(abs(get_evolutiontime(op)), tau):
+                    evolv_ops["W_tau"].append(op)
+                else:
+                    evolv_ops["W_tau_half"].append(op)
+    
 
         fig, ax = qml.draw_mpl(circuit)()
 
-        # Replace the default evolve label with a custom one
-        for artist in fig.findobj(Text):
-            txt = artist.get_text()
-            if "Exp(-"+str(np.round(tau/2, 2)) in txt:
+        exp_texts = [artist for artist in fig.findobj(Text) if "Exp(" in artist.get_text()]
+        evolve_ops_in_order = [op for op in all_ops if "Evolution" in op.name]
+
+
+
+        for artist, op in zip(exp_texts, evolve_ops_in_order):
+            if op in evolv_ops["H_env"]:
+                artist.set_text(r"$e^{i\omega\tau Z/2}$")
+            elif op in evolv_ops["W_tau_half"]:
                 artist.set_text(r"$W(\tau/2)$")
-            elif "Exp(-"+str(np.round(tau, 2)) in txt:
-                artist.set_text(r"$e^{-i\tau H_{\alpha}}$")      
-        fig.savefig("circuit.png", dpi=100, bbox_inches=None)
+            elif op in evolv_ops["W_tau"]:
+                artist.set_text(r"$W(\tau)$")
+            else:
+                artist.set_text(r"$e^{-i\tau H_{\mathrm{sys}}}$")
+
+        fig.savefig("circuit_"+str(num_timesteps)+"_steps.png", dpi=100, bbox_inches=None)
         plt.close(fig)
