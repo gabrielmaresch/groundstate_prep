ab211fd793c8b796e47e04b5fdcfda43b4ae3a86
Gabriel Maresch <gabriel.maresch@gmail.com>
2026-05-19 14:35:44 +0200

    add new analytics and cooling channel modules

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # groundstate_prep
-Implementation of ground- resp. thermal- state preparation algorithm following the quantum-channel approach presented in arXiv:2508.05703 from Ding et al. The project investigates cooling-based preparation methods for small quantum many-body systems, with a focus on toy models that are small enough for exact simulation and algorithmic testing.
+Implementation of ground- resp. thermal- state preparation algorithm following the quantum-channel approach presented in arXiv:2508.05703 from Ding et al. The project implements cooling-based preparation methods for small quantum many-body systems, with a focus on toy models that are small enough for exact diagonalization and algorithmic testing.
 
 Project for QIST master program at TU Wien, supervised by S. Andergassen and T. Ayral.
 
@@ -17,7 +17,7 @@ The implementation follows the quantum-channel approach to ground- and thermal-s
 
 2.) Implementation of cooling algorithm for 1+2 (impurity/bath) site DMFT model.
 
-## Questions:
+## Open Questions:
 - How does cooling work if run on a NISQ QC or with noise simulation?
 - Can we assign the noise an effective temperature and what T is necessary to be able to detect a particular phase-transition (e.g. superconducting phase)?
 - What computational budget is necessary to do so?
---
+++ analytics.py
@@ -0,0 +1,228 @@
+import pennylane as qml
+import pennylane.numpy as np
+import random
+import os
+import sys
+from pathlib import Path
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+from typing import Any, List
+from cooling_channel import create_circuit_diagram, create_cooling_circuit, transverse_ising_hamiltonian, construct_opset
+from scipy.optimize import curve_fit
+
+#__________________________________ JULIA ED ________________________________________
+from juliacall import Main as jl
+
+local_path = Path(__file__).resolve().parent
+file_name = "ed.jl" 
+jl.include(str(local_path/file_name))
+
+def get_gibbs(N, J, h, beta):
+    gibbs_state, energy = jl.get_transverse_ising_gibbsstate(N, J, h, beta)
+    return gibbs_state, energy
+
+def nice_print_matrix(A, digits=3):
+    A = np.asarray(A)
+    fmt = f"{{:.{digits}f}}"
+    for row in A:
+        print("  ".join(fmt.format(x) for x in row))
+
+def trace_distace(A,B):
+    lambdas = np.linalg.eigvalsh(A-B)
+    dist = 1/2*sum(abs(ev) for ev in lambdas)
+    return dist
+
+def exp_model(x, a, b, c):
+        return a+b*np.exp(c*x)
+
+def extract_asymptotics(x, y):
+
+    x_data = np.array(x)
+    y_data = np.array(y)
+
+    p_init = y_data[-1], y_data[0]-y_data[-1], -0.1
+    p_fit, cov = curve_fit(exp_model, x_data, y_data, p0=p_init)
+
+    y_fit = exp_model(x_data, *p_fit)
+
+    return y_fit, p_fit, cov
+
+
+
+#'____________________________ COOLING CICUIT EXECUTION _____________________________'
+
+def run_TFIM(H_params, T_params, C_params, beta, ops='XZ', *, timesteps=None, mixed=True, show_gibbs=False, silent=False, plots=True, seed=12):
+
+    assert len(H_params) == 3
+    J, h, N = H_params
+    H_sys  = transverse_ising_hamiltonian(J, h, N)
+    gibbs_state, energy = get_gibbs(N, J, h, beta)
+    
+
+    assert len(T_params) == 2 
+    T, tau = T_params
+
+    assert len(C_params) == 3 
+    alpha, sigma, omega_max = C_params
+
+    if ops in ['XZ', 'XX2']:
+        op_set = construct_opset(N, type=ops)
+
+    params = {
+    "N": N,
+    "J": J,
+    "h": h,
+    "beta": beta,
+    "T": T,
+    "tau": tau,
+    "alpha": alpha,
+    "sigma": sigma,
+    "omega_max": omega_max,
+    "ops": ops,
+    "mixed": mixed,
+    "seed": seed,
+    }
+
+    if not silent:
+        print("\n" + 80 * "-")
+        print("Start calculation")
+        print(80 * "-")
+
+        print("\nTransversal Ising XXZ model on", N,"qubits with J =", J, ", h =", h, ":\n")
+        print("H_sys =", H_sys, "\n")
+        if show_gibbs:
+            print(f"\nGibbs state for β = {beta}\n")
+            nice_print_matrix(gibbs_state)
+            print("\n")
+
+
+    if timesteps is None:
+        s = input("\nEnter number of max. timesteps [100]: ")
+        max_timesteps = int(s) if s else 100
+        start = 5
+        times = range(start, max(max_timesteps, start) + 1, 5) #minimum is 5 timesteps
+
+    elif isinstance(timesteps, int):
+        times = [timesteps]
+        max_timesteps = timesteps
+    elif isinstance(timesteps, (list, tuple, range, np.ndarray)):
+        max_timesteps = max(timesteps)
+        times = timesteps
+
+    first_run = True
+    tr_dist_gibbs = []
+    tr_dist_increment = [0]
+
+    for num_timesteps in times:
+        
+        circuit = create_cooling_circuit(
+            num_system_qubits=N,
+            num_timesteps=num_timesteps,
+            H_sys=H_sys,
+            tau=tau,
+            beta=beta,
+            T=T,
+            omega_max=omega_max,
+            op_set=op_set,
+            alpha=alpha,
+            sigma=sigma,
+            seed=seed,
+            mixed=mixed,
+            output=False)
+
+        final_state = circuit()
+        
+        #nice_print_matrix(final_state)
+        #print("\ntrace =", np.round(np.real(np.trace(final_state)),3))
+        #is_diagonal = np.allclose(final_state, np.diag(np.diagonal(final_state)))
+        #print("is diagonal:", is_diagonal)
+
+        dist = trace_distace(final_state, gibbs_state)
+        # print(num_timesteps, " timesteps:\t", np.round(dist,3), end ='\r')
+        if not silent:
+            print(num_timesteps, " timesteps:\t", np.round(dist,3)) #stable version for jupyter
+    
+
+        if not first_run:
+            dist_increment = trace_distace(previous_state, final_state)
+            if not silent:
+                print("\t\t\t\t\t trace dist. increment", np.round(dist_increment, 3))
+            tr_dist_increment.append(dist_increment)
+    
+        first_run = False
+        previous_state = final_state
+        tr_dist_gibbs.append(dist)
+
+    info_H = f"N = {N}\nJ = {J}\nh = {h}"
+    info_ch = (
+        f"$\\beta$ = {beta}\n"
+        f"$T$ = {T}\n"
+        f"$\\tau$ = {tau}\n"
+        f"$\\omega_{{\\max}}$ = {omega_max}\n"
+        f"$\\alpha$ = {alpha}\n"
+        f"$\\sigma$ = {sigma}"
+    )
+    if plots:
+    
+        plt.scatter(times, tr_dist_gibbs)
+        plt.xlabel("number of timesteps")
+        plt.title("trace distance cooled state vs. Gibbs state")
+
+        plt.text(
+            0.8, 0.95, info_H,
+            transform=plt.gca().transAxes,
+            verticalalignment="top",
+            horizontalalignment="left",
+            bbox=dict(boxstyle="round", facecolor="white", alpha=0.8))
+        plt.text(
+        0.8, 0.65, info_ch,
+        transform=plt.gca().transAxes,
+        verticalalignment="top",
+        horizontalalignment="left",
+        bbox=dict(boxstyle="round", facecolor="white", alpha=0.8)
+        )
+
+        dist_fitted, p_fit, cov = extract_asymptotics(times, tr_dist_gibbs)
+        plt.plot(times, dist_fitted, color="green", linestyle=":")
+        asymptotic_value = p_fit[0]
+        plt.text(0.5, 0.85, f"Asymptotic value = {asymptotic_value:.3f}",
+         transform=plt.gca().transAxes,
+         verticalalignment="top",
+         horizontalalignment="center")
+
+        pic_dir = "plots"
+        pic_name = "trace_distance_"+str(max_timesteps)+"timesteps.png"
+        plt.savefig(local_path/pic_dir/pic_name, dpi=200)
+        if not silent:
+            plt.show()
+
+
+    return np.array(times), np.array(tr_dist_gibbs), np.array(tr_dist_increment), params
+
+#############################################################################################
+
+
+
+if __name__ == "__main__":
+
+### H_params = [J, h, N]
+    H_params = [1., 2., 4]
+
+### T_params = [T, tau]
+    T_params = [1., 0.1]
+
+### C_params = [alpha, sigma, omega_max]
+    C_params = [0.25, 1.0, 6.0]
+
+#### inverse temperature
+    beta = 0.1
+
+
+    t, dist, _, _ = run_TFIM(H_params, T_params, C_params, beta)
+
+    dist_fitted, p_fit, cov = extract_asymptotics(t,dist)
+    print(p_fit, cov)
+
+# sys.stdout.flush()
